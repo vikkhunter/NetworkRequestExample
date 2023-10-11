58ba1fe569bf91f324576cf093e6198813f129f9
vivekalghurair <vivek.singh@al-ghurair.com>
2023-10-11 20:46:05 +0530

    ListView with array adapter

---
+++ app/src/main/java/com/example/networkrequestexample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.networkrequestexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.lv_names)
+
+        val listOfNames = arrayListOf<String>("Pawan", "Harsh", "Lucas")
+
+        val adapter = ArrayAdapter<String>(this, R.layout.list_item, listOfNames)
+
+        listView.adapter = adapter
+
+    }
+}
---
+++ app/src/main/java/com/example/networkrequestexample/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.networkrequestexample
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    fun getDataFromServer() {
+
+    }
+}
