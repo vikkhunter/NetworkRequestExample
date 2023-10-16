9b84b8cd0b9dbd6c4c3fb2016532a23b05c79cec
vikhunter <codeheister91@gmail.com>
2023-10-16 20:16:39 +0530

    Added style for text view

---
+++ app/src/main/java/com/example/networkrequestexample/CheckBoxesAndRadioButtonsExample.kt
@@ -0,0 +1,11 @@
+package com.example.networkrequestexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckBoxesAndRadioButtonsExample : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_boxes_and_radio_buttons_example)
+    }
+}
