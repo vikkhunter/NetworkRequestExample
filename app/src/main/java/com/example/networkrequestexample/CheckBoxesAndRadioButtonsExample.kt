28fbef5c63d3ef5777ee9449bce9d98d02e99877
vikhunter <codeheister91@gmail.com>
2023-10-16 20:36:09 +0530

    Added radio button example

--- app/src/main/java/com/example/networkrequestexample/CheckBoxesAndRadioButtonsExample.kt
+++ app/src/main/java/com/example/networkrequestexample/CheckBoxesAndRadioButtonsExample.kt
@@ -2,10 +2,21 @@ package com.example.networkrequestexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 class CheckBoxesAndRadioButtonsExample : AppCompatActivity() {
+
+    private val TAG = "CheckBoxesAndRadioButto"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_boxes_and_radio_buttons_example)
+
+        val radioGroup = findViewById<RadioGroup>(R.id.rg_answers)
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            Log.d(TAG, "onCreate: ${group.findViewById<RadioButton>(checkedId).text}")
+        }
     }
 }
