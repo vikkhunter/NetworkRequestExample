c72aa3d29855198603300d774b0388c0e1c32e8d
vikhunter <codeheister91@gmail.com>
2023-10-16 20:02:30 +0530

    DropDown box added from material design

--- app/src/main/java/com/example/networkrequestexample/SpinnerExampleActivity.kt
+++ app/src/main/java/com/example/networkrequestexample/SpinnerExampleActivity.kt
@@ -18,18 +18,17 @@ class SpinnerExampleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spinner_example)
-
-        val textInputLayout = findViewById<TextInputLayout>(R.id.textInputLayout)
         val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+
         val list = arrayListOf<String>("Item One", "Item Two", "Item three")
+
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, list)
+
         autoCompleteTextView.setAdapter(adapter)
         autoCompleteTextView.onItemClickListener =
             OnItemClickListener { _, _, position, _ ->
                 //do with selected item
                 Log.d("position", "onCreate: $position")
             }
-
-
     }
 }
